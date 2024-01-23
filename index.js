b7a798a5f14a0096573a7e35929741ad4b760ea7
gaganbansal21 <gagan@attryb.com>
2024-01-23 17:08:29 +0530

    second

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 5000;
   const MONGODB_URL = "mongodb://gaganbansal021:M04IK5DmmUCu6D62@ac-vgl0mge-shard-00-00.zdole43.mongodb.net:27017,ac-vgl0mge-shard-00-01.zdole43.mongodb.net:27017,ac-vgl0mge-shard-00-02.zdole43.mongodb.net:27017/?ssl=true&replicaSet=atlas-j1flgd-shard-0&authSource=admin&retryWrites=true&w=majority"
 
   mongoose
-  .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
